3e91a6fda5dd85deb0a097783f7e4002c8312b61
elvesbd <elvesbd@yahoo.com>
2023-06-29 20:04:34 -0300

    feat: create crud

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,53 @@ namespace Todo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public List<TodoModel> Get([FromServices] AppDbContext context)
+        public List<TodoModel> Get([FromServices] AppDbContext ctx)
         {
-            return context.Todos.ToList();
+            return ctx.Todos.ToList();
+        }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel Get([FromRoute] int id, [FromServices] AppDbContext ctx)
+        {
+            return ctx.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext ctx)
+        {
+            ctx.Todos.Add(todo);
+            ctx.SaveChanges();
+            return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext ctx
+        )
+        {
+            var model = ctx.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return todo;
+            }
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+            ctx.Todos.Update(model);
+            ctx.SaveChanges();
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Put([FromRoute] int id, [FromServices] AppDbContext ctx)
+        {
+            var model = ctx.Todos.FirstOrDefault(x => x.Id == id);
+
+            ctx.Todos.Remove(model);
+            ctx.SaveChanges();
+            return model;
         }
     }
 }
