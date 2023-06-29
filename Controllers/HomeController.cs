abe748bf2c88f7ed3ae147e6349ca4cd0d503b9c
elvesbd <elvesbd@yahoo.com>
2023-06-29 19:32:20 -0300

    feat: configured use controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public List<TodoModel> Get([FromServices] AppDbContext context)
+        {
+            return context.Todos.ToList();
+        }
+    }
+}
